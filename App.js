aad79a1ff8434e026cba7a5d96357edf912ce4bb
KhangNinh123 <ninhtrinh744@gmail.com>
2024-11-08 13:38:17 +0700

    add font

--- App.js
+++ App.js
@@ -1,120 +1,70 @@
-import React from 'react';
-import { View, Text, FlatList, Image, TouchableOpacity, StyleSheet } from 'react-native';
-
-const DATA = [
-  {
-    id: '1',
-    image: '', 
-    name: 'Ca nấu lẩu, nấu mì mini',
-    shop: 'Shop Devang',
-  },
-  {
-    id: '2',
-    image: '', 
-    name: '1KG KHÔ GÀ BƠ TỎI',
-    shop: 'Shop LTD Food',
-  },
-  {
-    id: '3',
-    image: '', 
-    name: 'Xe cần cẩu đa năng',
-    shop: 'Thế giới đồ chơi',
-  },
-  {
-    id: '4',
-    image: '', 
-    name: 'Đồ chơi dạng mô hình',
-    shop: 'Thế giới đồ chơi',
-  },
-  {
-    id: '5',
-    image: '', 
-    name: 'Lãnh đạo giản đơn',
-    shop: 'Minh Long Book',
-  },
-  {
-    id: '6',
-    image: '', 
-    name: 'Hiểu lòng con trẻ',
-    shop: 'Minh Long Book',
-  },
-  {
-    id: '7',
-    image: '', 
-    name: 'Donald Trump: Thiên tài lãnh đạo',
-    shop: 'Minh Long Book',
-  },
-];
+import { StatusBar } from 'expo-status-bar';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import { ClerkProvider,SignedIn, SignedOut, useAuth } from "@clerk/clerk-expo";
 
 
-const App = () => {
-  const renderItem = ({ item }) => (
-    <View style={styles.itemContainer}>
-      <Image source={{ uri: item.image }} style={styles.image} />
-      <View style={styles.textContainer}>
-        <Text style={styles.productName}>{item.name}</Text>
-        <Text style={styles.shopName}>{item.shop}</Text>
-      </View>
-      <TouchableOpacity style={styles.chatButton}>
-        <Text style={styles.chatText}>Chat</Text>
-      </TouchableOpacity>
-    </View>
-  );
+
+import * as SecureStore from "expo-secure-store";
+
+import { useState } from 'react';
+
+const tokenCache = {
+  async getToken(key) {
+    try {
+      return SecureStore.getItemAsync(key);
+    } catch (err) {
+      return null;
+    }
+  },
+  async saveToken(key, value) {
+    try {
+      return SecureStore.setItemAsync(key, value);
+    } catch (err) {
+      return;
+    }
+  },
+};
+export default function App() {
+  const [isChapterComplete,setIsChapterComplete]=useState(false);
+  const [userPoints,setUserPoints]=useState();
+  const [fontsLoaded] = useFonts({
+    'outfit': require('./assets/fonts/Outfit-Regular.ttf'),
+    'outfit-bold': require('./assets/fonts/Outfit-Bold.ttf'),
+    'outfit-medium': require('./assets/fonts/Outfit-SemiBold.ttf'),
+  });
 
   return (
+    <ClerkProvider 
+    tokenCache={tokenCache}
+    publishableKey={"Your Clerk Key"}>
+      <UserPointsContext.Provider value={{userPoints,setUserPoints}}>
+   <CompleteChapterContext.Provider value={{isChapterComplete,setIsChapterComplete}}>
     <View style={styles.container}>
-      <FlatList
-        data={DATA}
-        renderItem={renderItem}
-        keyExtractor={(item) => item.id}
-      />
+     
+      <SignedIn>
+          <NavigationContainer>
+            <TabNavigation/>
+          </NavigationContainer>
+          
+        </SignedIn>
+        <SignedOut>
+        <LoginScreen/>
+        </SignedOut>
     </View>
+
+    </CompleteChapterContext.Provider>
+    </UserPointsContext.Provider>
+    </ClerkProvider>
+    
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    paddingHorizontal: 10,
-  },
-  itemContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginVertical: 10,
-    padding: 10,
-    borderWidth: 1,
-    borderColor: '#ddd',
-    borderRadius: 10,
-    backgroundColor: '#fff',
-  },
-  image: {
-    width: 60,
-    height: 60,
-    borderRadius: 5,
-  },
-  textContainer: {
-    flex: 1,
-    marginLeft: 10,
-  },
-  productName: {
-    fontSize: 16,
-    fontWeight: 'bold',
-  },
-  shopName: {
-    fontSize: 14,
-    color: '#666',
-  },
-  chatButton: {
-    backgroundColor: '#ff0000',
-    paddingVertical: 10,
-    paddingHorizontal: 20,
-    borderRadius: 5,
-  },
-  chatText: {
-    color: '#fff',
-    fontWeight: 'bold',
+    marginTop:20
+   
   },
 });
-
-export default App;
